d57cd46c41631f5b76910c0aadb9b8ee639eb7cf
jorge <georgellanque@gmail.com>
2021-09-19 23:58:46 -0400

    Add changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import Pet from './Pet'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import SearchParams from './SearchParams'
+import Details from './Details'
 
 // const App = () => {
 //   return React.createElement(
@@ -29,8 +30,21 @@ import SearchParams from './SearchParams'
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to='/'>
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path='/details/:id'>
+            <Details />
+          </Route>
+          <Route>
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
