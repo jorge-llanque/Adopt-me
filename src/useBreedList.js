e19d7ada1eb495c1effa01fa459823c7ff29ef86
jorge <georgellanque@gmail.com>
2021-10-06 11:42:54 -0400

    Add new changes

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -17,7 +17,7 @@ export default function useBreedLIst(animal) {
       setBreedList([])
       setStatus('loading')
       const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+        `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
       )
       const json = await res.json()
       localCache[animal] = json.breeds || []
